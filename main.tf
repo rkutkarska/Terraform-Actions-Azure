05f1ae40553b1effe52bd3c20f4145fc64ef8c71
rkutkarska <ralena.dimitrova@gmail.com>
2023-06-17 10:33:10 +0300

    Add persistent storage

--- main.tf
+++ main.tf
@@ -7,6 +7,14 @@ terraform {
       version = "=3.59.0"
     }
   }
+
+# Save tfstate file
+  backend "azurerm" {
+    resource_group_name = "StorageRG"
+    storage_account_name = "taskboardstorageralena"
+    container_name = "taskboardcontainer"
+    key = "terraform.tfstate"
+  }
 }
 
 # Configure the Microsoft Azure Provider
